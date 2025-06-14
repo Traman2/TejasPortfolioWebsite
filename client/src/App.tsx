59842a8e6d3b7e149f7b4934c28c4c7832d3a2fa
Traman2 <tejassraman@gmail.com>
2025-06-13 22:10:01 -0500

    deleted server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 function App() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center" style={{ background: "#0F3462" }}>
-      <div className="p-4 rounded-xl shadow-lg flex flex-col items-center" style={{ background: "#1E6286" }}>
+    <div className="min-h-screen flex flex-col justify-center items-center bg-[#0F3462]">
+      <div className="p-14 rounded-xl shadow-lg flex flex-col items-center bg-[#1E6286]">
         <h1 className="text-5xl font-bold" style={{ color: "#CAF0F8", letterSpacing: "2px" }}>
           Coming Soon
         </h1>
         <p className="text-white text-lg mt-4 opacity-80">
-          Personal Website
+          Currently building the site. Please check back later
         </p>
       </div>
     </div>
