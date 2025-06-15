0a28edc3a740d57482fa237c042ebac6f07d1c14
Traman2 <tejassraman@gmail.com>
2025-06-14 21:47:08 -0500

    edits to website down

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState, useEffect } from 'react';
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { useState, useEffect } from 'react';
 
-import DesktopHome from "./pages/Desktop/Home";
-import DesktopProject from "./pages/Desktop/Projects";
-import DesktopContact from "./pages/Desktop/Contact";
+// import DesktopHome from "./pages/Desktop/Home";
+// import DesktopProject from "./pages/Desktop/Projects";
+// import DesktopContact from "./pages/Desktop/Contact";
 
-import MobileHome from "./pages/Mobile/MobileHome";
-import MobileProject from "./pages/Mobile/MobileProjects";
-import MobileContact from "./pages/Mobile/MobileContact";
+// import MobileHome from "./pages/Mobile/MobileHome";
+// import MobileProject from "./pages/Mobile/MobileProjects";
+// import MobileContact from "./pages/Mobile/MobileContact";
 import Maintenance from "./Maintaince";
 
 export default function App() {
-  const [isMobile, setIsMobile] = useState(false);
-  useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth < 768);
-    handleResize();
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+  // const [isMobile, setIsMobile] = useState(false);
+  // useEffect(() => {
+  //   const handleResize = () => setIsMobile(window.innerWidth < 768);
+  //   handleResize();
+  //   window.addEventListener('resize', handleResize);
+  //   return () => window.removeEventListener('resize', handleResize);
+  // }, []);
 
 
   // return isMobile ? (
