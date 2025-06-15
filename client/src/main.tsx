9902dd4ca3abc2b9019b4c945765112f1830b938
Traman2 <tejassraman@gmail.com>
2025-06-14 21:49:59 -0500

    added vercel speed insights

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
 
 inject();
+injectSpeedInsights();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
