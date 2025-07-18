f411329c08a2d5d8bc3bb383048035dd1908f6bf
Traman2 <tejassraman@gmail.com>
2025-07-18 04:27:58 -0500

    deleted client code in dev

--- client/src/Maintaince.tsx
+++
@@ -1,7 +0,0 @@
-export default function Maintenance() {
-    return (
-      <div className="min-h-screen flex items-center justify-center text-center text-2xl">
-        🚧 Site is under maintenance. Please check back soon!
-      </div>
-    );
-  }
