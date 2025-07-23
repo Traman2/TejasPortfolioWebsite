13855d2a11afc5aafd8c7d82a7e60dfd4391bcfa
Traman2 <tejassraman@gmail.com>
2025-07-23 05:37:51 -0500

    back track

--- client/src/Mobile/Home.tsx
+++ client/src/Mobile/Home.tsx
@@ -3,14 +3,11 @@ import MobileBottomNav from "@/components/Mobile/Home/BottomNav";
 import MobileHero from "@/components/Mobile/Home/Hero";
 import MobileSkills from "@/components/Mobile/Home/Skills";
 import { useEffect } from "react";
-import useMobileViewportHeight from "../hooks/useViewportHeight";
 
 export default function Home() {
     useEffect(() => {
       document.title = "Home - Tejas Raman";
     }, []);
-
-    useMobileViewportHeight();
   
     return (
       <div style={{ height: "calc(var(--vh, 1vh) * 100)" }} className="min-h-screen bg-[#0C1313] p-3 pb-18">
