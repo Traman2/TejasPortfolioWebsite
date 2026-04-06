150388bf631ef2b6b494d30d12eeca8132812f1a
Tejas Raman <134980520+Traman2@users.noreply.github.com>
2026-04-06 02:57:19 +0200

    Change model from 'gemini-2.5-flash-lite' to 'Gemma 4 31B'

--- server/services/ragChatbot.js
+++ server/services/ragChatbot.js
@@ -44,7 +44,7 @@ export const ragChatbot = async (query, history = []) => {
 
   // Langchain structured output LLM Call 
   const model = new ChatGoogleGenerativeAI({
-    model: "gemini-2.5-flash-lite",
+    model: "Gemma 4 31B",
     temperature: 0
   });
 
